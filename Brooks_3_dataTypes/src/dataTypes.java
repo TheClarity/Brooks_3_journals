8046e5178b62db06c2a9344095390c171c316c0f
TheClarity <colbybrooks17@yahoo.com>
2015-10-19 05:43:01 -0500

    Journals 10-19-15

---
+++ Brooks_3_dataTypes/src/dataTypes.java
@@ -0,0 +1,51 @@
+
+public class dataTypes {
+	public static void main (String [] args) {
+		int i = 0;
+		float a = 0.01f;
+		double b = 0.01;
+		short c = (short) .123;
+		char d =123;
+		String e = "words";
+		boolean f = true;
+		char d1 = ';';
+		char d2 ='a';
+		char d3 = 142;
+		char d4 = 42;
+		boolean g = 1 % 2 ==0;
+		/*
+		 here we will look at some mathematical operators
+		 */
+		// + adds two numbers and more
+		// - subtracts two numbers and more
+		// * this multiplies two numbers and more
+		// / this divides two numbers and more
+		// % this is modulo. Shows remainder
+		
+		
+		
+		System.out.println(i + " an integer");
+		System.out.println(a + " a float");
+		System.out.println(b + " a double");
+		System.out.println(c + " a short");
+		System.out.println(d1 +"something");
+		System.out.println(d2 +"something more");
+		System.out.println(d3 +"more");
+		System.out.println(d4 +"other more");
+		
+		
+		System.out.println(e + " a String");
+		System.out.println(e + " a String");
+		System.out.println(f + " a boolean");
+//		for(i = -123; i < 123; i++) {
+//			System.out.println((char) i);
+//			System.out.println( i % 3 == 0); 
+//		}
+		// a number value for char
+		//maps to a character
+		System.out.println(i + a + " an integer");
+		System.out.println(a + b + " a float");
+		System.out.println(b + c +" a double");
+		System.out.println(c + " a short");
+	}
+}
